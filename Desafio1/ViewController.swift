fb7cf76304cbd54c2c65e4fbc818a3aaa916d224
Fer Ugalde <fernando.ugalde.mtz@gmail.com>
2023-06-13 20:12:29 -0600

    Cambio de colores DarkMode, union de últimos ViewControllers

--- Desafio1/ViewController.swift
+++ Desafio1/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var DarkMode: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ChangeMode(_ sender: Any) {
+        if(sender as AnyObject).isOn{
+            print("prendido")
+            UIApplication.shared.keyWindow?.overrideUserInterfaceStyle = .dark
+        } else {
+            UIApplication.shared.keyWindow?.overrideUserInterfaceStyle = .light
+            print("apagado")
+            
+        }
+    }
+    
 }
 
